3983f710e31ceffd3f9ca0473cfa05fc7d3f1285
muhammedafreen <muhammadafreen20032@gmail.com>
2024-08-21 00:00:43 +0530

    Fourth  commit

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -2,9 +2,12 @@ import React from "react";
 import "./Item.css";
 
 function Item({ name, rating, price, saleDiscount, image, brand }) {
+  // Construct the full image path
+  const imageUrl = `${process.env.PUBLIC_URL}${image}`;
+
   return (
     <div className="item-card">
-      <img src={image} alt={"Item"} width="100%" />
+      <img src={imageUrl} alt={name} width="100%" />
       <div className="item-brand">{brand}</div>
       <div className="item-name">{name}</div>
       <div className="item-info">
